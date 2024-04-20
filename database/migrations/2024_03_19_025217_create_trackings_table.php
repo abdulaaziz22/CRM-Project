65337208eae2e7556df9995e72bb230b37435e37
awad3317 <awad3317@icloud.com>
2024-04-20 21:57:36 +0300

    Update 2024_03_19_025217_create_trackings_table.php

--- database/migrations/2024_03_19_025217_create_trackings_table.php
+++ database/migrations/2024_03_19_025217_create_trackings_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('trackings', function (Blueprint $table) {
             $table->id();
-            $table->date('startdate');
             $table->date('enddate');
             $table->string('details');
             $table->string('subject');
