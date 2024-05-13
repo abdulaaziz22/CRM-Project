4e510f51db0205aa12e16716e6152eb06c9c910c
awad3317 <127610663+awad3317@users.noreply.github.com>
2024-05-13 22:23:59 +0300

    add middleware notification

--- app/Http/Middleware/MarkNotificationAsRead.php
+++ app/Http/Middleware/MarkNotificationAsRead.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class MarkNotificationAsRead
@@ -14,7 +15,15 @@ class MarkNotificationAsRead
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response
-    {
+    {  
+        $notify_id=$request->query('notify_id');
+        dd($notify_id);
+        if($notify_id){
+            $notification=Auth::user()->unreadNotifications()->find($notify_id);
+            if($notification){
+                $notification->markAsRead();
+            }
+        }
         return $next($request);
     }
 }
